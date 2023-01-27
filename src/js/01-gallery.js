8943ba07aa862eb1fa34757b9b933e3f0f4d4d4c
Vitaliya Ovechko <ovechko.vita@gmail.com>
2023-01-27 22:07:47 +0200

    Виконує 1 завдання ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallery(gallery) {
+    return gallery.map(({ preview, original, description }) => 
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    ).join("");
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, captionPosition:'bottom', scrollZoom: false,});
