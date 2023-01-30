e6afbc033d2792e4c557b7dceede34592cd57a6c
Vitaliya Ovechko <ovechko.vita@gmail.com>
2023-01-30 09:20:01 +0200

    Завдання №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const ifrem = document.querySelector('iframe');
+const player = new Player(ifrem);
+
+console.log(player)
+
+const onPlay = function (evt) {
+    const currentTime = evt.seconds;
+    console.log(currentTime);
+    localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const timeFromLocalStorage = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(JSON.parse(timeFromLocalStorage)).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
